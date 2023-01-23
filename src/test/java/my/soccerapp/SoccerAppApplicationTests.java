4c8ac363d2f19901fc0e594cfd95f4d7882884b2
Andrey Zinchenko <astrokoit@gmail.com>
2023-01-24 01:08:19 +0700

    Init commit

---
+++ src/test/java/my/soccerapp/SoccerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my.soccerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
